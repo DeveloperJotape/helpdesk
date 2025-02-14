10db507453fce275dd5b968487fa40fc11908e35
Joao Pedro <developerjotape@gmail.com>
2025-02-14 12:13:26 -0300

    fix: adicionada a anotação de repository

--- src/main/java/br/com/devjoaopedro/helpdesk/repository/TicketRepository.java
+++ src/main/java/br/com/devjoaopedro/helpdesk/repository/TicketRepository.java
@@ -4,9 +4,11 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.devjoaopedro.helpdesk.entity.Ticket;
 
+@Repository
 public interface TicketRepository extends JpaRepository<Ticket, UUID> {
 
     // Busca chamados por titulo (ignora letras maiúsculas e minúsculas)

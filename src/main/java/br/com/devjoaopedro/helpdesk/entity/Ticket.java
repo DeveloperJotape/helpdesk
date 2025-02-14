defecd3ae72058061c50fad8e1494328a50fbdcd
Joao Pedro <developerjotape@gmail.com>
2025-02-14 11:29:28 -0300

    feat: ticket entity adicionado

--- src/main/java/br/com/devjoaopedro/helpdesk/entity/Ticket.java
+++ src/main/java/br/com/devjoaopedro/helpdesk/entity/Ticket.java
@@ -6,7 +6,17 @@ import java.util.UUID;
 
 import org.apache.tomcat.util.http.parser.Priority;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -15,13 +25,33 @@ import lombok.Data;
 @Data
 public class Ticket {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
     private String title;
     private String description;
-    private LocalDateTime startDate;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy HH:mm:ss")
+    @Column(name = "start_date")
+    private LocalDateTime startDate = LocalDateTime.now();
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy HH:mm:ss")
+    @Column(name = "end_date")
     private LocalDateTime endDate;
+
+    @ManyToOne
+    @JoinColumn(name = "requestor_id")
+    private Employee requester;
+
+    @ManyToOne
+    @JoinColumn(name = "requested_id")
+    private Employee requested;
+
+    @Enumerated(EnumType.STRING)
     private Priority priority;
+
+    @Enumerated(EnumType.STRING)
     private Status status;
-    private Employee employee;
 
 }

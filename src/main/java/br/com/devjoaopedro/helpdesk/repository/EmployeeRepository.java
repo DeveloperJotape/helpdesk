d38318b38d576a43c0e75c83fca19ae82f684740
Joao Pedro <developerjotape@gmail.com>
2025-02-14 12:12:31 -0300

    feat: adicionados métodos de busca por nome

--- src/main/java/br/com/devjoaopedro/helpdesk/repository/EmployeeRepository.java
+++ src/main/java/br/com/devjoaopedro/helpdesk/repository/EmployeeRepository.java
@@ -1,5 +1,6 @@
 package br.com.devjoaopedro.helpdesk.repository;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,5 +10,8 @@ import br.com.devjoaopedro.helpdesk.entity.Employee;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, UUID> {
+
+    // Busca funcionários por nome (ignora letras maiúsculas e minúsculas)
+    List<Employee> findByNameContainingIgnoreCase(String name);
 
 }
--- src/main/java/br/com/devjoaopedro/helpdesk/repository/TicketRepository.java
+++ src/main/java/br/com/devjoaopedro/helpdesk/repository/TicketRepository.java
@@ -1,11 +1,15 @@
 package br.com.devjoaopedro.helpdesk.repository;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.devjoaopedro.helpdesk.entity.Ticket;
 
-public interface TicketRepository extends JpaRepository<UUID, Ticket> {
+public interface TicketRepository extends JpaRepository<Ticket, UUID> {
+
+    // Busca chamados por titulo (ignora letras maiúsculas e minúsculas)
+    List<Ticket> findByTitleContainingIgnoreCase(String title);
 
 }

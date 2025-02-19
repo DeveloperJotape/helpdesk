7f493bbb2fefa7e7ae38dd70fe47250757059bb3
Joao Pedro <developerjotape@gmail.com>
2025-02-19 09:31:55 -0300

    fix: description, priority e status adicionados no dto

--- src/main/java/br/com/devjoaopedro/helpdesk/dto/TicketCreateDTO.java
+++ src/main/java/br/com/devjoaopedro/helpdesk/dto/TicketCreateDTO.java
@@ -1,13 +1,16 @@
 package br.com.devjoaopedro.helpdesk.dto;
 
 import br.com.devjoaopedro.helpdesk.entity.enums.Priority;
+import br.com.devjoaopedro.helpdesk.entity.enums.Status;
 
 import java.util.UUID;
 
 public record TicketCreateDTO(
         String title,
+        String description,
         UUID requester,
         UUID requested,
-        Priority priority) {
+        Priority priority,
+        Status status) {
 
 }

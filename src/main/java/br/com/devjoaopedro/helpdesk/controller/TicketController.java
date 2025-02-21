b7f317bd0f32d44117ac8d82e5f2c56863ad3969
Joao Pedro <developerjotape@gmail.com>
2025-02-21 11:34:59 -0300

    feat: CrossOrigin adicionado

--- src/main/java/br/com/devjoaopedro/helpdesk/controller/TicketController.java
+++ src/main/java/br/com/devjoaopedro/helpdesk/controller/TicketController.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -28,6 +29,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 @RestController
 @RequestMapping("/api/tickets")
 @Tag(name = "Chamados", description = "API para gerenciamento de chamados")
+@CrossOrigin(origins = "*")
 public class TicketController {
 
     @Autowired
